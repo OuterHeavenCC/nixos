738c362a07cc2bffe89814d482bfc6514e57449c
Corentin CHAMINAND <corent.cham@gmail.com>
2023-06-25 14:42:32 +0200

    Ajout de la configuration du touchpad

--- hosts/deimOS/sysconfig/display.nix
+++ hosts/deimOS/sysconfig/display.nix
@@ -5,6 +5,12 @@
     enable = true;
     layout = "fr";
     displayManager.lightdm.enable = false;
+    libinput.touchpad = {
+      enable = true;
+      middleEmulation = true;
+      tapping = true;
+      disableWhileTyping = true;
+    };
   };
   hardware = {
     opengl.enable = true;
