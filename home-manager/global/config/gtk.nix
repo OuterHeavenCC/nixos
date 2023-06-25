5376cefd6173ddc4f4fdbb417cfffc4b4a007e92
Corentin CHAMINAND <corent.cham@gmail.com>
2023-06-25 16:31:27 +0200

    Typo

--- home-manager/global/config/gtk.nix
+++ home-manager/global/config/gtk.nix
@@ -1,16 +1,14 @@
-{ config, pkgs, ... }:
+{config, pkgs, ... }:
 
 {
   gtk = {
     enable = true;
     theme = {
       name = "catppuggin-gtk";
-      package = "pkgs.catppuccin-gtk";
-      };
+      package = pkgs.catppuccin-gtk;
+    };
     gtk2 = {
-      configLocation = {
-        "${config.xdg.configHome}/gtk-2.0/gtkrc";
-      };
+      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
     };
     gtk4 = {
       extraConfig = {
