7ccd38f9b1f1e87afb10761bc454345bee68b322
Corentin CHAMINAND <corent.cham@gmail.com>
2023-07-08 18:44:58 +0200

    Website

--- hosts/common/optional/nginx/nginx.nix
+++ hosts/common/optional/nginx/nginx.nix
@@ -1,5 +1,4 @@
 {
-
   networking.firewall = {
     allowedTCPPortRanges = [
       { from = 80; to = 80; }
@@ -8,10 +7,15 @@
 
   services.nginx = {
     enable = true;
-    virtualHosts."46.226.104.146" = {
+    virtualHosts."chaminand.com" = {
+      enableACME = true;
+      forceSSL = true;
       root = "/var/www/chaminand.com";
     };
   };
   # Optional: You can configure the email address used with Let's Encrypt.
   # This way you get renewal reminders (automated by NixOS) as well as expiration emails.
+  security.acme.certs = {
+  "chaminand.com".email = "corent.cham@gmail.com";
+  };
 }
