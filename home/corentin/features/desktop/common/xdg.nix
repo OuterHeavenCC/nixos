67087f2b94a3cf1afa2df66159de2a845bbf1352
Corentin CHAMINAND <corent.cham@gmail.com>
2023-07-18 15:04:50 +0200

    Added torrent desktop entry for xdg

--- home/corentin/features/desktop/common/xdg.nix
+++ home/corentin/features/desktop/common/xdg.nix
@@ -1,13 +1,23 @@
 { config, ... }:
 
 {
-    xdg.userDirs = {
-      enable = true;
-      download = "${config.home.homeDirectory}/Téléchargements";
-      music = "${config.home.homeDirectory}/Musique";
-      pictures = "${config.home.homeDirectory}/Images";
-      videos = "${config.home.homeDirectory}/Vidéos";
-      templates = "${config.home.homeDirectory}/Modèles";
+    xdg = {
+        userDirs = {
+          enable = true;
+          download = "${config.home.homeDirectory}/Téléchargements";
+          music = "${config.home.homeDirectory}/Musique";
+          pictures = "${config.home.homeDirectory}/Images";
+          videos = "${config.home.homeDirectory}/Vidéos";
+          templates = "${config.home.homeDirectory}/Modèles";
+          };
+
+        desktopEntries = {
+          torrent = {
+            type = "Application";
+            Name = "Torrent";
+            Exec= "/usr/bin/env transadd %U";
+          };
+        };
     };
 }
 
