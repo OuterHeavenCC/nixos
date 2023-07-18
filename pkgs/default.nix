520b3c9ade1ca6c7247742b9df76fafa295f5daa
Corentin CHAMINAND <corent.cham@gmail.com>
2023-07-19 00:20:37 +0200

    added bmks as custom package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
+  bmks = pkgs.callPackage ./bmks { };
   # example = pkgs.callPackage ./example { };
 }
