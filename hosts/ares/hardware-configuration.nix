254fa6bdaabfe4bb6f5fd1fa993cdcc11cb78a72
Corentin CHAMINAND <corent.cham@gmail.com>
2023-07-18 12:08:14 +0200

    test

--- hosts/ares/hardware-configuration.nix
+++ hosts/ares/hardware-configuration.nix
@@ -35,7 +35,7 @@
     system.stateVersion = "23.05";
 
     fileSystems."/" =
-      { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
+      { device = "/dev/disk/by-label/nixos";
         fsType = "ext4";
       };
 
