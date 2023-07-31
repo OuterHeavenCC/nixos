cf0a9e794d787d8d49fb99cb936e2a25a9780283
Corentin CHAMINAND <corent.cham@gmail.com>
2023-07-31 22:02:22 +0200

    ledger_entry snippet

--- home/corentin/features/cli/lvim/luasnippets/all.lua
+++ home/corentin/features/cli/lvim/luasnippets/all.lua
@@ -42,7 +42,6 @@ return {
         }
       )
     ),
-
     s("plan_adulte", 
       fmt(
         [[
@@ -192,5 +191,25 @@ return {
             }
           )
         ),
+        s("ledger_entry", 
+      fmt(
+        [[
+          20{}-{}-{} {}
+              Revenu:{}:Cabinet {}:Consultation {}    -{}€ ; Payee: {}
+              Actifs:CMB:Professionnel:Courant
+        ]],
+          {
+          i(1, "YY"),
+          i(2, "MM"),
+          i(3, "DD"),
+          i(4, "N° FACTURE"),
+          i(5, "MOYEN DE PAIEMENT"),
+          i(6, "CABINET"),
+          i(7, "ADULTE/ENFANT"),
+          i(8, "PRIX"),
+          i(9, "NOM Prénom"),
+          }
+        )
+      ),
 
 }
