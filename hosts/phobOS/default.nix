be11559845b5968e789949c5a21083c798e5f96c
Corentin CHAMINAND <corent.cham@gmail.com>
2023-08-14 19:06:04 +0200

    added default amd cpu config

--- hosts/phobOS/default.nix
+++ hosts/phobOS/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
 {
   # You can import other NixOS modules here
   imports = [
+
+    inputs.hardware.nixosModules.common-cpu-amd
 
     ../common/global
     ../common/users/corentin
