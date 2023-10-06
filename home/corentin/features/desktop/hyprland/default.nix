662e77c1e2623510ac5a34d3663e458f7b26f368
Corentin CHAMINAND <corent.cham@gmail.com>
2023-10-06 10:16:24 +0200

    switched to hyprland git

--- home/corentin/features/desktop/hyprland/default.nix
+++ home/corentin/features/desktop/hyprland/default.nix
@@ -9,6 +9,7 @@ in {
 
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     settings = {
       exec-once = [
         "wbg ${cfg.dataHome}/bg"
