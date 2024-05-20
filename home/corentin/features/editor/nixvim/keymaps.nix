ed617032dd3800496c8b25a1a0f306b7fdab6864
Corentin CHAMINAND <corent.cham@gmail.com>
2024-05-20 16:27:17 +0200

    more keymapping

--- home/corentin/features/editor/nixvim/keymaps.nix
+++ home/corentin/features/editor/nixvim/keymaps.nix
@@ -1,14 +1,68 @@
 {
   programs.nixvim.keymaps = [
+    # Better indenting
     {
         key = "<";
-        mode = "v";
         action = "<gv";
+        mode = "v";
     }
     {
       key = ">";
-      mode = "v";
       action = ">gv";
+      mode = "v";
+    }
+    # Better window movement
+    {
+      key = "<C-h>";
+      action = "<C-w>h";
+      mode = "n";
+    }
+    {
+      key = "<C-j>";
+      action = "<C-w>j";
+      mode = "n";
+    }
+    {
+      key = "<C-k>";
+      action = "<C-w>k";
+      mode = "n";
+    }
+    {
+      key = "<C-l>";
+      action = "<C-w>l";
+      mode = "n";
+    }
+    # Resize with arrows
+    {
+      key = "<C-Up>";
+      action = ":resize -2<CR>";
+      mode = "n";
+    }
+    {
+      key = "<C-Down>";
+      action = ":resize +2<CR>";
+      mode = "n";
+    }
+    {
+      key = "<C-Left>";
+      action = ":vertical resize -2<CR>";
+      mode = "n";
+    }
+    {
+      key = "<C-Right>";
+      action = ":vertical resize -2<CR>";
+      mode = "n";
+    }
+    # Move current line / block with Alt-j/k
+    {
+      key = "<A-j>";
+      action = ":m '>+1<CR>gv-gv";
+      mode = "x";
+    }
+    {
+      key = "<A-k>";
+      action = ":m '<-2<CR>gv-gv";
+      mode = "x";
     }
   ];
 }
